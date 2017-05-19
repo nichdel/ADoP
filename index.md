0120296a825e49e65397b1dea01452c3a65d16ec
nch <nichdel@gmail.com>
2017-05-18 23:59:18 -0500

    another

--- index.md
+++ index.md
@@ -1,4 +1,8 @@
 [The next (unofficial) report](Reports/next.txt) 
+
 [The latest report](Reports/next.txt) 
+
 The rest: 
+
 [2017-05-18.txt](Reports/2017-05-18.txt) 
+
--- make_index.py
+++ make_index.py
@@ -11,8 +11,12 @@ links.reverse()
 
 f = open('index.md', 'w')
 
-f.write("[The next (unofficial) report](" + next + ") \r\n")
-f.write("[The latest report](" + folder + links.pop(0) + ") \r\n")
-f.write("The rest: \r\n")
+f.write("[The next (unofficial) report](" + next + ") \n")
+f.write("\n")
+f.write("[The latest report](" + folder + links.pop(0) + ") \n")
+f.write("\n")
+f.write("The rest: \n")
+f.write("\n")
 for string in links:
-   f.write("[" + string + "](" + folder + string + ") \r\n")
+   f.write("[" + string + "](" + folder + string + ") \n")
+   f.write("\n")
